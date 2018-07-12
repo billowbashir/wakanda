c5aafb1057eb221b2483349c0303eb405878b17e
billowbashir <billowbashir@gmail.com>
2018-07-12 12:34:14 +0300

    succesfuly added two inputted no.s separated by +

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
+
+
+
+
+
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var noString = $("input#input1").val();
-    var noArray = parseInt(noString.split(","));
-    
+    var noArray = noString.split("+");
+    var sum=0;
+     for(var number=0;number<=noArray.length-1;number+=1){
+       sum+=parseInt(noArray[number])
+     };
+    console.log(sum)
   });
 });
